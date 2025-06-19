7613262b330048c6b721dbbb3b12fc9661beaa78
sjmy <23250749+sjmy@users.noreply.github.com>
2025-06-18 22:32:11 -0700

    Circle buttons working and styled

--- src/image-carousel.js
+++ src/image-carousel.js
@@ -1,3 +1,5 @@
+import prevButtonImage from "./img/arrow_back.svg";
+import nextButtonImage from "./img/arrow_forward.svg";
 import imgOne from "./img/david-schultz-qMS4NnsZTY0-unsplash.jpg";
 import imgTwo from "./img/jw-k-EY_hc694pm0-unsplash.jpg";
 import imgThree from "./img/radek-skrzypczak-ds0vFWE7q6Q-unsplash.jpg";
@@ -12,25 +14,52 @@ export default function imageCarouselObject() {
       const imageElement = document.createElement("img");
 
       imageElement.src = rawImages[i];
+      imageElement.classList.add("carouselImage");
+      imageElement.classList.add(`image${i}`);
       imageContainer.appendChild(imageElement);
     }
   }
 
-  function drawCarouselButtons() {
-    const prevButton = document.createElement("button");
-    const nextButton = document.createElement("button");
-    const pageImages = document.querySelectorAll("img");
+  function drawCarouselNavButtons() {
+    const prevDiv = document.createElement("div");
+    const nextDiv = document.createElement("div");
+    const prevButton = document.createElement("img");
+    const nextButton = document.createElement("img");
+    const carouselImages = document.querySelectorAll(".carouselImage");
 
+    prevDiv.classList.add("prevDiv");
+    nextDiv.classList.add("nextDiv");
     prevButton.classList.add("prevButton");
     nextButton.classList.add("nextButton");
+    prevButton.src = prevButtonImage;
+    nextButton.src = nextButtonImage;
+    prevDiv.appendChild(prevButton);
+    nextDiv.appendChild(nextButton);
 
-    pageImages[0].before(prevButton);
-    pageImages[pageImages.length - 1].after(nextButton);
+    carouselImages[0].before(prevDiv);
+    carouselImages[carouselImages.length - 1].after(nextDiv);
+  }
+
+  function drawCarouselCircleButtons() {
+    const circleButtonsDiv = document.createElement("div");
+    const body = document.querySelector("body");
+    const carouselImages = document.querySelectorAll(".carouselImage");
+
+    for (let i = 0; i < carouselImages.length; i++) {
+      const circleButton = document.createElement("div");
+      circleButton.classList.add("circleButton");
+      circleButton.classList.add(`image${i}`);
+      circleButtonsDiv.appendChild(circleButton);
+    }
+
+    circleButtonsDiv.classList.add("circleButtonsDiv");
+    body.appendChild(circleButtonsDiv);
   }
 
   function addButtonListeners() {
-    const prevButton = document.querySelector(".prevButton");
-    const nextButton = document.querySelector(".nextButton");
+    const prevButton = document.querySelector(".prevDiv");
+    const nextButton = document.querySelector(".nextDiv");
+    const circleButtons = document.querySelectorAll(".circleButton");
 
     nextButton.addEventListener("click", () => {
       nextImage();
@@ -39,19 +68,52 @@ export default function imageCarouselObject() {
     prevButton.addEventListener("click", () => {
       prevImage();
     });
+
+    for (let i = 0; i < circleButtons.length; i++) {
+      circleButtons[i].addEventListener("click", (e) => {
+        const imageNum = e.target.classList[1];
+        showImage(imageNum);
+        circleButtons[i].classList.add("active");
+      });
+    }
+  }
+
+  function showImage(imageNum) {
+    const carouselImages = document.querySelectorAll(".carouselImage");
+    const circleButtons = document.querySelectorAll(".circleButton");
+
+    for (let i = 0; i < carouselImages.length; i++) {
+      if (carouselImages[i].classList.contains("show")) {
+        carouselImages[i].classList.remove("show");
+      }
+
+      if (carouselImages[i].classList.contains(imageNum)) {
+        carouselImages[i].classList.add("show");
+      }
+    }
+
+    for (let i = 0; i < circleButtons.length; i++) {
+      if (circleButtons[i].classList.contains("active")) {
+        circleButtons[i].classList.remove("active");
+      }
+
+      if (circleButtons[i].classList.contains(imageNum)) {
+        circleButtons[i].classList.add("active");
+      }
+    }
   }
 
   function prevImage() {
-    const pageImages = document.querySelectorAll("img");
+    const carouselImages = document.querySelectorAll(".carouselImage");
 
-    for (let i = 0; i < pageImages.length; i++) {
-      if (pageImages[i].classList.contains("show")) {
-        pageImages[i].classList.remove("show");
+    for (let i = 0; i < carouselImages.length; i++) {
+      if (carouselImages[i].classList.contains("show")) {
+        carouselImages[i].classList.remove("show");
 
         if (i === 0) {
-          pageImages[pageImages.length - 1].classList.add("show");
+          carouselImages[carouselImages.length - 1].classList.add("show");
         } else {
-          pageImages[i - 1].classList.add("show");
+          carouselImages[i - 1].classList.add("show");
         }
         break;
       }
@@ -59,16 +121,16 @@ export default function imageCarouselObject() {
   }
 
   function nextImage() {
-    const pageImages = document.querySelectorAll("img");
+    const carouselImages = document.querySelectorAll(".carouselImage");
 
-    for (let i = 0; i < pageImages.length; i++) {
-      if (pageImages[i].classList.contains("show")) {
-        pageImages[i].classList.remove("show");
+    for (let i = 0; i < carouselImages.length; i++) {
+      if (carouselImages[i].classList.contains("show")) {
+        carouselImages[i].classList.remove("show");
 
-        if (i === pageImages.length - 1) {
-          pageImages[0].classList.add("show");
+        if (i === carouselImages.length - 1) {
+          carouselImages[0].classList.add("show");
         } else {
-          pageImages[i + 1].classList.add("show");
+          carouselImages[i + 1].classList.add("show");
         }
         break;
       }
@@ -76,13 +138,17 @@ export default function imageCarouselObject() {
   }
 
   function startCarousel() {
-    const firstImage = document.querySelector("img");
-    firstImage.classList.add("show");
+    const firstCarouselImage = document.querySelector(".carouselImage");
+    const firstCircleButton = document.querySelector(".circleButton");
+
+    firstCarouselImage.classList.add("show");
+    firstCircleButton.classList.add("active");
   }
 
   function start() {
     populateImageContainer();
-    drawCarouselButtons();
+    drawCarouselCircleButtons();
+    drawCarouselNavButtons();
     addButtonListeners();
     startCarousel();
   }
