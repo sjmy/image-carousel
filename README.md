ad3d6e9a15d108fcdbda31f6ee009546b2b1e43b
sjmy <23250749+sjmy@users.noreply.github.com>
2025-06-19 18:00:43 -0700

    Slideshow moves every five seconds. README update, comments.

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # image-carousel
 
-Image carousel assignment for The Odin Project.
+Image carousel assignment for The Odin Project. Click arrows for next/previous image. Click the dots to select a specific image. Carousel automatically moves to the next image every five seconds.
--- src/image-carousel.js
+++ src/image-carousel.js
@@ -5,7 +5,10 @@ import imgTwo from "./img/jw-k-EY_hc694pm0-unsplash.jpg";
 import imgThree from "./img/radek-skrzypczak-ds0vFWE7q6Q-unsplash.jpg";
 import imgFour from "./img/spencer-plouzek-9MBK-eldjIQ-unsplash.jpg";
 
+// All images are added to a single div (image-container), and selected via the CSS class "show" which activates the image display.
+// Image display is set to "none" by default; "show" is added when necessary.
 export default function imageCarouselObject() {
+  // image-container div is populated with imported images. CSS classes are added to each image.
   function populateImageContainer() {
     const imageContainer = document.querySelector(".image-container");
     const rawImages = [imgOne, imgTwo, imgThree, imgFour];
@@ -20,6 +23,7 @@ export default function imageCarouselObject() {
     }
   }
 
+  // Draws previous and next buttons with imported icons. CSS classes are added. Icons are placed in divs and overlayed on the images using CSS.
   function drawCarouselNavButtons() {
     const prevDiv = document.createElement("div");
     const nextDiv = document.createElement("div");
@@ -40,6 +44,8 @@ export default function imageCarouselObject() {
     carouselImages[carouselImages.length - 1].after(nextDiv);
   }
 
+  // Draws circle buttons (divs with a CSS border-radius) and appends them to the body.
+  // Each circle button has the same image# CSS class as each image
   function drawCarouselCircleButtons() {
     const circleButtonsDiv = document.createElement("div");
     const body = document.querySelector("body");
@@ -56,28 +62,30 @@ export default function imageCarouselObject() {
     body.appendChild(circleButtonsDiv);
   }
 
+  // Buttons listeners. Circle buttons grab their image# class to show the correct image.
   function addButtonListeners() {
     const prevButton = document.querySelector(".prevDiv");
     const nextButton = document.querySelector(".nextDiv");
     const circleButtons = document.querySelectorAll(".circleButton");
 
-    nextButton.addEventListener("click", () => {
-      nextImage();
-    });
-
     prevButton.addEventListener("click", () => {
       prevImage();
+    });
+
+    nextButton.addEventListener("click", () => {
+      nextImage();
     });
 
     for (let i = 0; i < circleButtons.length; i++) {
       circleButtons[i].addEventListener("click", (e) => {
         const imageNum = e.target.classList[1];
         showImage(imageNum);
-        circleButtons[i].classList.add("active");
       });
     }
   }
 
+  // Activated when a circle button is clicked. I think there is a way to consolidate some code between
+  // showImage, nextImage, and prevImage. I'm repeating myself.
   function showImage(imageNum) {
     const carouselImages = document.querySelectorAll(".carouselImage");
     const circleButtons = document.querySelectorAll(".circleButton");
@@ -105,6 +113,7 @@ export default function imageCarouselObject() {
 
   function prevImage() {
     const carouselImages = document.querySelectorAll(".carouselImage");
+    const circleButtons = document.querySelectorAll(".circleButton");
 
     for (let i = 0; i < carouselImages.length; i++) {
       if (carouselImages[i].classList.contains("show")) {
@@ -118,10 +127,24 @@ export default function imageCarouselObject() {
         break;
       }
     }
+
+    for (let i = 0; i < circleButtons.length; i++) {
+      if (circleButtons[i].classList.contains("active")) {
+        circleButtons[i].classList.remove("active");
+
+        if (i === 0) {
+          circleButtons[circleButtons.length - 1].classList.add("active");
+        } else {
+          circleButtons[i - 1].classList.add("active");
+        }
+        break;
+      }
+    }
   }
 
   function nextImage() {
     const carouselImages = document.querySelectorAll(".carouselImage");
+    const circleButtons = document.querySelectorAll(".circleButton");
 
     for (let i = 0; i < carouselImages.length; i++) {
       if (carouselImages[i].classList.contains("show")) {
@@ -135,9 +158,24 @@ export default function imageCarouselObject() {
         break;
       }
     }
+
+    for (let i = 0; i < circleButtons.length; i++) {
+      if (circleButtons[i].classList.contains("active")) {
+        circleButtons[i].classList.remove("active");
+
+        if (i === circleButtons.length - 1) {
+          circleButtons[0].classList.add("active");
+        } else {
+          circleButtons[i + 1].classList.add("active");
+        }
+        break;
+      }
+    }
   }
 
-  function startCarousel() {
+  // Initializes the carousel by attaching "show" to the first image and "active" to the first circle button.
+  // I chose different classes for each to style them independently.
+  function initCarousel() {
     const firstCarouselImage = document.querySelector(".carouselImage");
     const firstCircleButton = document.querySelector(".circleButton");
 
@@ -145,12 +183,19 @@ export default function imageCarouselObject() {
     firstCircleButton.classList.add("active");
   }
 
+  // Start the slideshow and move to the next image every five seconds.
+  function startSlideshow() {
+    setInterval(nextImage, 5000);
+  }
+
+  // Start process. Draw the HTML, add the listeners, show the first image, start the slideshow.
   function start() {
     populateImageContainer();
     drawCarouselCircleButtons();
     drawCarouselNavButtons();
     addButtonListeners();
-    startCarousel();
+    initCarousel();
+    startSlideshow();
   }
 
   return { start };
